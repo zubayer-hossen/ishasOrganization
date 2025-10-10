1fa28e1b7537db6cbed8092a86c72786dc686c36
Zubayer Hossen <fzhossen143r@gmail.com>
2025-10-10 17:58:42 +0600

    apiClientUrlChange

--- client/src/api.js
+++ client/src/api.js
@@ -2,7 +2,7 @@
 import axios from "axios";
 
 // ✅ Base URL Configuration
-const API_URL = import.meta.env.VITE_API_URL || "http://localhost:5000/api";
+const API_URL = import.meta.env.VITE_API_URL || 'https://ishasorganizationbackend.onrender.com/api';
 
 // ✅ 1. Main axios instance তৈরি
 const api = axios.create({
